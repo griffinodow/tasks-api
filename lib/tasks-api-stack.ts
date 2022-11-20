e542e093c1e75b252368701bdced731e66d0016a
griffinodow <griffin@griffindow.com>
2022-11-19 16:59:14 -0800

    Init repo

--- lib/tasks-api-stack.ts
+++ lib/tasks-api-stack.ts
@@ -1,16 +1,40 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { DomainName, EndpointType } from "aws-cdk-lib/aws-apigateway";
+import { DnsValidatedCertificate } from "aws-cdk-lib/aws-certificatemanager";
+import { CnameRecord, HostedZone } from "aws-cdk-lib/aws-route53";
+import { Construct } from "constructs";
 
 export class TasksApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Constants
+    const domainName = "griffindow.com";
+    const subDomainName = `api.tasks.${domainName}`;
+    const zone = HostedZone.fromLookup(this, "Zone", {
+      domainName: domainName,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TasksApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Domain
+    const certificate = new DnsValidatedCertificate(
+      this,
+      "TasksApiCertificate",
+      {
+        domainName: subDomainName,
+        hostedZone: zone,
+        region: "us-east-1",
+      }
+    );
+    const customDomain = new DomainName(this, "TasksApiDomain", {
+      domainName: subDomainName,
+      certificate: certificate,
+      endpointType: EndpointType.REGIONAL,
+    });
+
+    new CnameRecord(this, "ApiGatewayRecordSet", {
+      zone: zone,
+      recordName: subDomainName,
+      domainName: customDomain.domainNameAliasDomainName,
+    });
   }
 }
